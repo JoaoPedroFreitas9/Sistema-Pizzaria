80c89e3f56b60cf177c2bddc00f647bbef59a215
JoaoPedroFreitas9 <sr300546@gmail.com>
2025-08-13 20:22:29 -0300

    Adiciona controlador e serviço para listar pedidos

--- src/routes.ts
+++ src/routes.ts
@@ -15,6 +15,7 @@ import { RemoveOrderController } from "./controllers/order/RemoveOrderController
 import { AddItemController } from "./controllers/order/AddItemController";
 import { RemoveItemController } from "./controllers/order/RemoveItemController";
 import { SendOrderController } from "./controllers/order/SendOrderController";
+import { ListOrdersController } from "./controllers/order/ListOrdersController";
 
 import uploadConfig from "./config/multer";
 
@@ -60,5 +61,6 @@ router.delete(
   new RemoveItemController().handle
 );
 router.put("/order/send", isAuthenticated, new SendOrderController().handle);
+router.get("/orders", isAuthenticated, new ListOrdersController().handle);
 
 export { router };
